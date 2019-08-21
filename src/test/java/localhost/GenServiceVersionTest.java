8e48b64a88e5bc3890bd4cfd4ab2ae98525646c7
Marek Kuczabinski <m.kuczabinski@radytek.com>
2019-08-21 08:35:56 +0200

    ECFEED-705 Test for getting gen service versions added.

---
+++ src/test/java/localhost/GenServiceVersionTest.java
@@ -0,0 +1,48 @@
+package localhost;
+
+import com.ecfeed.core.webservice.client.GenWebServiceClient;
+import com.ecfeed.core.webservice.client.IWebServiceClient;
+import com.ecfeed.core.webservice.client.WebServiceResponse;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class GenServiceVersionTest {
+
+    @Test
+    public void getVersionTest() {
+
+        IWebServiceClient genWebServiceClient = createWebServiceClient();
+
+        WebServiceResponse webServiceResponse = genWebServiceClient.sendGetRequest();
+
+        if (!webServiceResponse.isResponseStatusOk()) {
+            fail();
+        }
+
+        BufferedReader bufferedReader = webServiceResponse.getResponseBufferedReader();
+
+        try {
+            bufferedReader.readLine();
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
+    }
+
+
+    private IWebServiceClient createWebServiceClient() { // TODO - similar method in RemoteTCProviderTest
+
+        Optional<String> keyStorePath = Optional.of("src/test/resources/security");
+
+        return new GenWebServiceClient(
+                "https://localhost:8090", // TODO
+                GenWebServiceClient.getGenServiceVersionEndPoint(),
+                "localTestRunner", // TODO
+                keyStorePath);
+    }
+
+}
