e15af71e0983c5b6d76702623e82c0cbf36811fe
Marek Kuczabinski <m.kuczabinski@radytek.com>
2020-03-02 13:21:18 +0100

    Test for exported JSON added.

--- src/test/java/localhost/GenerationWithExportTest.java
+++ src/test/java/localhost/GenerationWithExportTest.java
@@ -4,11 +4,16 @@ import com.ecfeed.core.webservice.client.GenWebServiceClient;
 import com.ecfeed.core.webservice.client.IWebServiceClient;
 import com.ecfeed.core.webservice.client.WebServiceResponse;
 import localhost.utils.TestHelper;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
+import java.util.stream.Collectors;
 
-import static localhost.utils.TestHelper.REQUEST_DATA;
+import static java.lang.Integer.parseInt;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class GenerationWithExportTest {
@@ -24,27 +29,70 @@ public class GenerationWithExportTest {
 
         WebServiceResponse webServiceResponse = genWebServiceClient.sendPostRequest(TestHelper.REQUEST_EXPORT, request);
 
+        verifyResponse(webServiceResponse);
+    }
+
+    private void verifyResponse(WebServiceResponse webServiceResponse) {
+
         if (!webServiceResponse.isResponseStatusOk()) {
             fail();
         }
 
-        BufferedReader bufferedReader = webServiceResponse.getResponseBufferedReader();
+        JSONObject jsonObject = convertToJsonObject(webServiceResponse);
 
+        // get array of test cases
+
+        JSONArray jsonArray = null;
         try {
-
-            for(;;) {
-                String line = bufferedReader.readLine();
-
-                if (line == null) {
-                    break;
-                }
-
-                System.out.println(line);
-            }
-        } catch (Exception e) {
-            fail(e.getMessage());
+            jsonArray = jsonObject.getJSONArray("testCases");
+        } catch (JSONException e) {
+            fail();
         }
 
+        // check test cases
+
+        JSONObject jsonObjectRow = null;
+        try {
+            for (int testCaseIndex = 0; testCaseIndex < jsonArray.length(); testCaseIndex++) {
+
+                jsonObjectRow = jsonArray.getJSONObject(testCaseIndex);
+                verifyResponseRow(testCaseIndex, jsonObjectRow);
+            }
+        } catch (JSONException e) {
+            fail();
+        }
+    }
+
+    private void verifyResponseRow(int testCaseIndex, JSONObject jsonObjectRow) throws JSONException {
+
+        String testCaseIndexStr = jsonObjectRow.getString("index"); // TODO - convert index in JSON to number instead of String
+        int resultTestCaseIndex = Integer.parseInt(testCaseIndexStr);
+        assertEquals(testCaseIndex, resultTestCaseIndex);
+
+        String arg1 = jsonObjectRow.getString("arg1");
+        if (!arg1.startsWith("V")) {
+            fail();
+        }
+
+        String arg2 = jsonObjectRow.getString("arg2");
+        if (!arg2.startsWith("V")) {
+            fail();
+        }
+    }
+
+    private JSONObject convertToJsonObject(WebServiceResponse webServiceResponse) {
+
+        BufferedReader bufferedReader = webServiceResponse.getResponseBufferedReader();
+        String result = bufferedReader.lines().collect(Collectors.joining());
+
+        JSONObject jsonObject = null;
+
+        try {
+            jsonObject = new JSONObject(result);
+        } catch (JSONException err) {
+            fail();
+        }
+        return jsonObject;
     }
 
 }
